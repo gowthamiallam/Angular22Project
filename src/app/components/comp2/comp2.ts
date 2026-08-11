d5033b0766bc4c5c32681264e7ff4df047fce430
Gowthami <gowthamiallam123@gmail.com>
2026-08-12 04:34:39 +0530

    behavioursubject

--- src/app/components/comp2/comp2.ts
+++ src/app/components/comp2/comp2.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { SharedService } from '../shared/shared-service';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-comp2',
@@ -13,7 +14,10 @@ export class Comp2 implements OnInit {
 
 
   ngOnInit(): void {
-    this.getData();
+    // this.getData();
+    // this.observableConcept();
+    // this.subjectConcept();
+    this.behaviourSubjectConcept();
   }
 
   getData() {
@@ -23,6 +27,57 @@ export class Comp2 implements OnInit {
     this.shareservice.user$.subscribe(res => {
       console.log(res, "COMPONENT 222");
     });
+
+  }
+
+  observableConcept() {
+    //Observable will not emit same data in case of random sometimes
+
+    let myobservable = new Observable((observer) => {
+      // observer.next(Math.random()); //for random 
+      observer.next(1);
+      observer.next(2); //for static
+    })
+
+
+    myobservable.subscribe((data) => { console.log(data, "TEST-obs") });    //Subscriber 1 or component 1
+
+    myobservable.subscribe((data) => { console.log(data, "TEST1-obs",) });    //Subscriber 2 or component 2
+  }
+  subjectConcept() {
+    //SUBJECT DOES NOT HOLD THE INITIAL VALUE ONCE THE NEW VALUE IS EMITTED THE NEW SUBSCRIBER DOES NOT GET THE LATEST VALUE
+    //Subject will emit same data 
+
+    const subject = new Subject();
+
+    subject.subscribe((data) => { console.log(data, "TEST-sub") });    //Subscriber 1 or component 1
+
+    subject.subscribe((data) => { console.log(data, "TEST1-sub") });  //Subscriber 2 or component 2
+
+    // subject.next(Math.random()); for random
+    subject.next(2026);   //EMITTING NEW VALUE
+
+
+    subject.subscribe((data) => { console.log(data, "TEST2-sub") });
+
+  }
+
+  behaviourSubjectConcept() {
+    //BEHAVIOUR HOLD THE INITIAL VALUE WHEN THERE IS NO NEW VALUE EMIT FOR A SUBSCRIBER
+    const behaviourSubject = new BehaviorSubject<number>(100);
+
+    behaviourSubject.subscribe((data) => { console.log(data, "SUBSCRIBER :1") }); //Subscriber 1 or component 1
+
+    behaviourSubject.subscribe((data) => { console.log(data, "SUBSCRIBER :2") }); //Subscriber 2 or component 2
+
+    behaviourSubject.next(2020);
+
+    //NEW SUBSCRIBER HOLD THE LATEST EMITTED VALUE NOT AN INITIAL VALUE
+
+    behaviourSubject.subscribe((data) => { console.log(data, "SUBSCRIBER :3") });
+
+    behaviourSubject.next(2023);
+
 
   }
 }
